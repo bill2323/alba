bf6ef635066bc61f7ea6be3676f24849a63886c7
Amz <tengo16414@gmail.com>
2017-05-16 20:42:26 +0400

    bla

---
+++ Assets/Scripts/LevelBuilder.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBuilder : MonoBehaviour
+{
+	public Transform player;
+	public GameObject[] platforms;
+	public int numberOfStartingPlatforms;
+	public float destroyTime = 5f; //destroy platforms after some time
+	public Vector3 platformSpawnPoint = Vector3.zero;
+
+	//-------------------- Unity Functions: ------------------------------------
+	void Start()
+	{
+		int counter = 0;
+
+		//create starting platforms
+		while (counter < numberOfStartingPlatforms)
+		{
+			CreateNextPlatform ();
+			counter = counter + 1;
+		}
+	}
+
+	int pp;
+	int lastPP = 0;
+	void Update()
+	{
+		//number of 50 meter segments passed
+		pp = (int) player.position.z / 50;
+
+		//if moving to a new segment
+		if (pp > lastPP) 
+		{
+			//create a new platform
+			CreateNextPlatform ();
+		}
+
+		lastPP = pp;
+	}
+
+	//-------------------- My Custom Functions ------------------------------------
+	void CreateNextPlatform()
+	{
+		Debug.Log ("Creating Platform");
+
+		//move spawn position on z axis
+		platformSpawnPoint.z = platformSpawnPoint.z + 50f;
+
+		//randomly choose a platfrom
+		int platformChooser = Random.Range(0, platforms.Length);
+
+		//create a new GameObject
+		GameObject clone;
+		clone = Instantiate ( platforms[ platformChooser ] );
+
+		//move the new GameObject to spawn position
+		clone.transform.position = platformSpawnPoint;
+
+		//destroy the object after some time
+		Destroy (clone, destroyTime);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{         
+	public AudioSource sfxAudioPlayer;
+	public Rigidbody myBody;
+
+	public AudioClip[] audioFiles;
+
+	public float maxSpeed;
+	public float moveAcceleration;
+	public float jumpAcceleration;
+	private bool isGrounded = false;
+	private bool isGameOver = false;
+
+	public float gameOverHeight = -5f;
+
+	public float rayCastDistance = 1f;
+
+	void FixedUpdate ()
+	{
+		GroundChecker ();
+		ConstantMove ();
+	}
+
+	void Update()
+	{
+
+
+		if (transform.position.y < gameOverHeight && isGameOver == false){
+			isGameOver = true;
+			sfxAudioPlayer.PlayOneShot (audioFiles [2]);
+
+		}
+
+
+
+		if (isGrounded && (Input.GetKeyDown (KeyCode.Space) ||  Input.GetMouseButtonDown(0)))
+		{
+			Jump ();
+		}
+
+		Debug.DrawLine (transform.position, transform.position + Vector3.down * rayCastDistance);
+	}
+
+	void OnCollisionEnter(Collision collisionInfo)
+	{  
+		sfxAudioPlayer.clip = audioFiles [0];
+		sfxAudioPlayer.Play();
+
+	}
+
+	void OnCollisionExit(Collision collisionInfo)
+	{
+		
+	}
+
+
+	void ConstantMove()
+	{
+		Vector3 newVelocity = myBody.velocity;
+
+		//maqsimalurze meti agar wava
+	
+		if (newVelocity.z >= maxSpeed)
+		{
+			newVelocity.z = maxSpeed;
+		}
+
+		//maqsimaluri siswrafe
+		else
+		{
+			newVelocity.z = newVelocity.z + moveAcceleration;
+		}
+
+		myBody.velocity = newVelocity;
+	}
+	void GroundChecker(){
+		Ray ray = new Ray ();
+		ray.origin = transform.position;
+		ray.direction = Vector3.down;
+
+		isGrounded = Physics.Raycast (ray, rayCastDistance);
+		//Debug.DrawLine (transform.position, transform.position + Vector3.down * rayCastDistance);
+	}
+
+	void Jump()
+	{
+
+		Vector3 jumpVelocity = myBody.velocity;
+		jumpVelocity.y = jumpVelocity.y + jumpAcceleration;
+		myBody.velocity = jumpVelocity;
+		sfxAudioPlayer.PlayOneShot (audioFiles [1]);
+	}
+}
